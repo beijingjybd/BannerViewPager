b8bc3412ccd78b5469ce33047039f5eacec7e609
王杜康 <wangdukang@jybdshop.cn>
2020-10-15 17:12:45 +0800

    修改打包配置

--- app/src/main/java/com/example/zhpan/circleviewpager/App.java
+++ app/src/main/java/com/example/zhpan/circleviewpager/App.java
@@ -2,6 +2,8 @@ package com.example.zhpan.circleviewpager;
 
 import android.app.Application;
 
+import androidx.multidex.MultiDex;
+
 import com.tencent.bugly.crashreport.CrashReport;
 import com.zhpan.bannerview.utils.BannerUtils;
 import com.zhpan.idea.utils.Utils;
@@ -17,6 +19,8 @@ public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        // 主要是添加下面这句代码
+        MultiDex.install(App.this);
         Utils.init(getApplicationContext());
         BannerUtils.setDebugMode(true);
         CrashReport.initCrashReport(getApplicationContext(), "69b176a2b6", true);
